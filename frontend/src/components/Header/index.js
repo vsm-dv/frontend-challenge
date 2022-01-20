f5ba6f8db34c7e1caf7a37cddab920adae0c95c0
vsm-dv <valeria.mouraaa@gmail.com>
2022-01-20 19:31:25 -0300

    Resolução desafio front-end ajuste ícone busca

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,5 +1,4 @@
 import './styles.css'
-import searchIcon from '../../assets/search-icon.svg';
 
 function Header({ searchTerm, setSearchTerm }) {
     return (
@@ -9,7 +8,6 @@ function Header({ searchTerm, setSearchTerm }) {
                 placeholder='Pesquisar'
                 onChange={(e) => setSearchTerm(e.target.value)}
                 value={searchTerm} />
-            <img src={searchIcon} alt="Pesquisar" className='search-icon' />
             <span>Olá Bianca!</span>
         </header>
     )
